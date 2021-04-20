c02ca4e93b2e245a8a51df3f679bc5d6b5ab8768
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-04-20 22:01:55 +0900

    6.1 room admin panel 2

--- rooms/models.py
+++ rooms/models.py
@@ -78,7 +78,7 @@ class Room(core_models.TimeStampedModel):
     room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
     amenities = models.ManyToManyField("Amenity", blank=True)
     facilities = models.ManyToManyField("Facility", blank=True)
-    houserules = models.ManyToManyField("HouseRule", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
