cd9fd6665d1c58bfae27696118917cc0d0aa69b8
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-05-27 18:52:33 +0900

    7.3 finishing the room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -8,6 +8,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -55,6 +60,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -74,6 +80,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
