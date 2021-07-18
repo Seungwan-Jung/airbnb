7298242e91b7c5bd04413b379db7a5e499eb2931
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-07-19 01:53:26 +0900

    12.4 using detailview CMV

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ")=0db80xqc%45$1(f*puw4^mhnuj-zzd=#n&#y&g0v)uu(i=@x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,6 @@
 from . import models
-from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import ListView
-from django.urls import reverse
+from django.views.generic import ListView,DetailView
+
 # Create your views here.
 
 class HomeView(ListView):
@@ -15,10 +13,6 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name="rooms"
 
-def room_detail(request,pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request,"rooms/detail.html",{"room":room})
-    except models.Room.DoesNotExist:
-        raise Http404()        
-        
+class RoomDetail(DetailView):
+    """ Room Detail Definition"""        
+    model = models.Room
