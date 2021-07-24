d4834a55787508e77e09ca9a64c5ee9450a69e2e
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-07-25 03:23:14 +0900

    13.5 filtering like a boss part one

--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from django_countries import countries
 
+import rooms
+
 # Create your views here.
 
 class HomeView(ListView):
@@ -61,4 +63,16 @@ def search(request):
         "facilities":facilities,
     }
 
-    return render(request,"rooms/search.html",{**form,**choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type !=0:
+        filter_args["room_type__pk"] = room_type
+    
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request,"rooms/search.html",{**form,**choices,"rooms":rooms})
