151956c00c1d4e8030de0bb136373b31b254e878
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-08-06 02:11:05 +0900

    15.0 signup form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect, reverse
-from django.views import View
+from django.shortcuts import redirect, reverse
 from . import forms
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import FormView
@@ -22,3 +21,9 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "murphy","last_name":"jung","email":"murphy@django.com"}
